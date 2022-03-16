bffabb348ece40f62adab33e6972ffcda5e15a92
Alejandro © <acocac@gmail.com>
2022-03-16 11:39:58 +0000

    enforce python 3.7

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     url="https://github.com/MartinSJRogers/VEdge_Detector",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires="=3.7",
+    python_requires="~=3.7",
 )
